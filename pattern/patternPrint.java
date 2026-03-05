1f6820565540439cbc89f826872bedc090d3d2b8
Janani Mohandass <jananimohandass24@gmail.com>
2026-03-05 15:31:31 +0530

    Refactor patternPrint to print 4 rows instead of 9
    Updated the pattern printing logic to print fewer rows and corrected the loop conditions.

--- pattern/patternPrint.java
+++ pattern/patternPrint.java
@@ -1,8 +1,8 @@
 class patternPrint{
     public static void main(String args[]){
         int k=1;
-        for(int i=1;i<10;i++){
-            for(int j=1;j<=i && k<=10;j++){
+        for(int i=1;i<5;i++){
+            for(int j=1;j<=i;j++){
                 System.out.print(k+ " ");
                 k++;
             }
@@ -10,4 +10,5 @@ class patternPrint{
         System.out.println(" ");
     }
 }
+
 }
